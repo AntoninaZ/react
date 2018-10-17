3bfdab6ef031c6fa395aa44d06a109e8c7f84939
Antonina <zuch21@gmail.com>
2018-10-17 18:38:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {
+  Component
+} from 'react';
+
+import './App.css'
+
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper"> 
+        <Header/>
+      </div>);
+  }
+}
+
+export default App;
